aa56a636cce18ae87e35e97c4cc9a4af2a4233d3
OuttaSpace <outtaspace888@gmail.com>
2024-03-01 15:21:44 -0800

    Start of calculator

---
+++ constants.py
@@ -0,0 +1,8 @@
+"""
+All constants used for dialysis project
+accumulated in one file for easier accessibility 
+and cleaner code.
+"""
+
+# Window settings and all colors used in project
+MAIN_FRAME_COLOR = "#f4efeb"
---
+++ dialysis.py
@@ -0,0 +1,91 @@
+import tkinter as tk
+from tkinter import ttk
+from tkmacosx import Button as button
+import requests
+import constants
+
+# Lifting page to the top
+class Page(tk.Frame):
+    # Main content frame
+    def __init__(self):
+        tk.Frame.__init__(self, bg=constants.MAIN_FRAME_COLOR)
+
+    def show(self):
+        self.lift()
+
+# Welcome page! First page of app
+class Intro(Page):
+    intro_text = (
+        "Here you can find recommended foods to eat for dialysis patients as well as which foods to avoid. You can also find nutrition information for individual food items of your choice with the built in API!\n"
+        "\nUse the calculator to determine your daily energy and protein intake for a balanced diet.\n"
+        "\nCheck out the other tabs for additional useful information that can help you consume your favorite foods in a safe way!"
+    )
+    # App Header
+    def __init__(self):
+        Page.__init__(self)
+        label = tk.Label(
+            self,
+            text="Welcome to the Dialysis Nutrition App!",
+            bg=constants.MAIN_FRAME_COLOR,
+            font=("Arial", 24, "bold"),
+        )
+        label.pack(pady=30)
+
+        # Border around text body and message generation
+        border_frame = tk.LabelFrame(self, bg=constants.MAIN_FRAME_COLOR)
+        border_frame.pack()
+        message = tk.Message(
+            border_frame,
+            text=Intro.intro_text,
+            font=("Arial", 18),
+            bg=constants.MAIN_FRAME_COLOR,
+        )
+        message.pack(pady=20)
+
+class Calculator(Page):
+    def __init__(self):
+        Page.__init__(self)
+        label = tk.Label(
+            self,
+            text="Calculate your daily energy and protein intake requirements.",
+            font=("Arial", 20),
+            bg=constants.MAIN_FRAME_COLOR,
+        )
+        label.pack(pady=25)
+
+        # Enter weight, label
+        weight_label = tk.Label(
+            self,
+            text="Enter your weight in kg:",
+            font=("Arial", 18),
+            bg=constants.MAIN_FRAME_COLOR,
+        )
+        weight_label.pack(pady=10)
+
+        # Weight entry box
+        weight_entry = tk.Entry(self)
+        weight_entry.pack(pady=10)
+
+        # Weight submit button
+        weight_btn = button(
+            self,
+            text="Submit",
+            bg="#5ddeef",
+            activebackground="#4285f4",
+            font=("Arial", 14),
+            command=lambda: self.calculated_values(weight_entry.get()),
+        )
+        weight_btn.pack(pady=10)
+
+        # additional note marked with '*'
+        note = tk.Label(
+            self,
+            text="*All numbers are rounded to the nearest ones place",
+            bg=constants.MAIN_FRAME_COLOR,
+        )
+        note.place(relx=0.05, rely=0.9)
+
+    def calculated_values(self, weight):
+        calories = self.displaying_calories(weight)
+        protein = self.displaying_protein(weight)
+
